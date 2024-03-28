5d683615081e35a446844888711286095280f571
naliali <nalialiangela365@gmail.com>
2024-03-28 11:36:39 +0300

    event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-function addingEventListener() {
+//let button = document.getElementById('button')
+//button.addEventListener("Click Me!",() => {
+    //button.innerHTML = "Click Me!"
+    //alert = 
+//})
+
+//const button = document.getElementById("button")
+
+//unction clickAlert(){
+    //alert = "Click Me!"
+//}
+
+//input.addingEventListener('click', clickAlert)
+function addingEventListener(){
+    const button = document.getElementById("button");
+
+    function handleClick(event){
+        alert = "Click Me!"
+    }
+    button.addEventListener("Click Me!",handleClick)
+
 }
+
